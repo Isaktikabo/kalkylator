32c7fa2263a2317abeae721a71b3b5f143a0eb03
Isak Tikabo <70146067+Isaktikabo@users.noreply.github.com>
2020-11-30 15:22:21 +0100

    kalkylator

--- kalkylatorn.py
+++ kalkylatorn.py
@@ -1,23 +1,23 @@
-run = 5:
+run = 0
 
-while run = 0:
-print('[1] räkna med +')
-print('[2] räkna med -')
-print('[3] räkna med *')
-print('[4] valfritt arbete, om du vill')
-print('[5] avsluta')
+while run !=5:
+    print('[1] räkna med +')
+    print('[2] räkna med -')
+    print('[3] räkna med *')
+    print('[4] valfritt arbete, om du vill')
+    print('[5] avsluta')
 
-run = int(input('gör ett val: '))
+    run = int(input('gör ett val: '))
 
-if run = 1:
-    print('\nräkna med addition')
-    tal1 = float(input('skriv ett tal: '))
-    tal2 = float(input('skriv ett andra tal: '))
-    summa = tal1 + tal2
-    print(f'summa av {tal1} plus {tal2} är {summa}')
-elif run = 3
-   print ('\nräkna med multiplikation')
-   tal1 = float(input('skriv ett tal:'))
-   tal2 = float(input('skirv ett andra tal:'))
-   summa = tal1 * tal2
-print (f'summa av {tal1} * {tal2} är {summa}\n')
+    if run == 1:
+        print('\nräkna med addition')
+        tal1 = float(input('skriv ett tal: '))
+        tal2 = float(input('skriv ett andra tal: '))
+        summa = tal1 + tal2
+        print(f'summa av {tal1} plus {tal2} är {summa}')
+    elif run == 3:
+        print ('\nräkna med multiplikation')
+        tal1 = float(input('skriv ett tal:'))
+        tal2 = float(input('skirv ett andra tal:'))
+        summa = tal1 * tal2
+        print (f'summa av {tal1} * {tal2} är {summa}\n')
